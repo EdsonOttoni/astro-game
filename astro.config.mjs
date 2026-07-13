ad72c0d466562364f22cb8c6d2c91ac9af5ed96b
Edson Ottoni <43525138+EdsonOttoni@users.noreply.github.com>
2026-07-13 20:06:50 -0300

    Update base URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://edsonottoni.github.io',
-  base: '/astro-game/',
+  base: 'https://saruearcano.com.br',
   vite: {
     plugins: [tailwindcss()]
   }
